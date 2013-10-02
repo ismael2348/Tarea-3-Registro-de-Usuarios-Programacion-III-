9e1b4a9ce590461bf35e362821162bccd5689d8e
ismael <mayel_d-_-b_@hotmail.com>
2013-10-02 18:01:32 -0500

    Regitro de Usuarios

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        
+{
+        public string nombre;
+        public string correo;
+        public int edad;
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("Escribe Tu Nombre");
+            p.nombre = Console.ReadLine();
+            Console.WriteLine("Escribe Tu Correo");
+            p.correo = Console.ReadLine();
+            Console.WriteLine("Escribe Tu Edad");
+            p.edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n Mi Nombre Es: " + p.nombre + "\n Mi Correo Es: " + p.correo + "\n Mi Edad Es: " + p.edad + "Años");
+            Console.Read();
+        }
+    }
+}
+       
+    
+}
